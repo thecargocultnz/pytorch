b90af715e145555904c55ff00549bd70c571420f
angelayi <yiangela7@gmail.com>
2025-04-29 10:56:16 -0700

    [export] Refactor pt2 save/load

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -92,7 +92,6 @@ from torch.fx.experimental.symbolic_shapes import has_hint, hint_int, ShapeEnv
 from torch.utils._ordered_set import OrderedSet
 
 from .output_code import CompiledFxGraph
-from .package.pt2_archive_constants import CUSTOM_OBJ_FILENAME_PREFIX
 from .remote_cache import create_cache
 from .runtime import autotune_cache
 from .runtime.autotune_cache import AutotuneCacheBundler
@@ -1896,6 +1895,10 @@ class AotCodeCompiler:
                 ):
                     constant = constant.real_obj
                 assert isinstance(constant, torch._C.ScriptObject)
+                from torch.export.pt2_archive._constants import (
+                    CUSTOM_OBJ_FILENAME_PREFIX,
+                )
+
                 custom_obj_name = f"{CUSTOM_OBJ_FILENAME_PREFIX}{custom_obj_idx}"
 
                 log.debug("saving script object %s as %s", name, custom_obj_name)
